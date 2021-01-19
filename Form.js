da9ef6c5f842b3577cb046f6fbdc9d4b7ee213f4
alex <fennyflop@gmail.com>
2021-01-19 16:32:23 +0300

    big code

--- Form.js
+++ Form.js
@@ -1,8 +1,19 @@
 export default class Form {
-    constructor (quantity, maxNum, systems) {
-        this._quantity = +quantity;
-        this._maxNum = +maxNum;
-        this._systems = systems;
+    constructor (selector, selector2) {
+        this._selector = selector;
+        this._formElement = document.querySelector(this._selector);
+        this._quantity = +this._formElement.querySelector('#quantity').value;
+        this._maxNum = +this._formElement.querySelector('#max').value;
+        this._systems = this._formElement.querySelector('#systems').value;
+        
+        this._selector2 = selector2;
+        this._formElement2 = document.querySelector(this._selector2);
+        this._quantity2 = +this._formElement2.querySelector('#quantity').value;
+        this._maxNum2 = +this._formElement2.querySelector('#max').value;
+        this._systems2 = this._formElement2.querySelector('#systems').value;
+    }
+    log () {
+        console.log(this._formElement2);
     }
     renderJson() {
         let systemsArr = [];
@@ -11,12 +22,36 @@ export default class Form {
                 systemsArr.push(+e);
             }
         })
+        let systemsArr2 = [];
+        this._systems2.split(",").forEach((e) => {
+            if (e != '') {
+                systemsArr2.push(+e);
+            }
+        })
         return JSON.stringify(
             {
-                quantity: this._quantity,
-                maxNum: this._maxNum,
-                systems: systemsArr,
+                    to10 : {
+                        quantity: this._quantity2,
+                        maxNum: this._maxNum2,
+                        systems: systemsArr2,
+                    },
+                from10 : {
+                    quantity: this._quantity,
+                    maxNum: this._maxNum,
+                    systems: systemsArr,
+                }
             }
+            // {
+                // quantity: this._quantity,
+                // maxNum: this._maxNum,
+                // systems: systemsArr.map((e) => {
+                //     if (e != '') {
+                //         return +e;
+                //     } else {
+                //         return;
+                //     }
+                // }),
+            // }
         )
     }
 }
--- index.js
+++ index.js
@@ -1,19 +1,58 @@
+import FormQ from './FormQ.js';
 import Form from './Form.js';
-const nonbinaryForm = document.querySelector('#nonbinary');
-const formAnswer = nonbinaryForm.querySelector('.form__answer');
+import Api from './Api.js';
+const form10tox = document.querySelector('#from10-x');
+const fromxto10 = document.querySelector('#fromx-10');
+const registration = document.querySelector('#registration');
+const toX = document.querySelector('#toX');
+const to10 = document.querySelector('#to10');
+const toReg = document.querySelector('#toReg');
+const formAnswer = form10tox.querySelector('.form__answer');
 
-nonbinaryForm.addEventListener('submit', (evt) => {
+const api = new Api ({
+    baseUrl: 'http://0c1b8c566344.ngrok.io',
+    
+});
+
+toX.addEventListener('click', () => {
+    form10tox.classList.remove('none');
+    registration.classList.add('none');
+    fromxto10.classList.add('none');
+    toX.classList.add('select__button-selected');
+    to10.classList.remove('select__button-selected');
+    toReg.classList.remove('select__button-selected');
+});
+
+to10.addEventListener('click', () => {
+    fromxto10.classList.remove('none');
+    form10tox.classList.add('none');
+    registration.classList.add('none');
+    toX.classList.remove('select__button-selected');
+    to10.classList.add('select__button-selected');
+    toReg.classList.remove('select__button-selected');
+});
+
+toReg.addEventListener('click', () => {
+    fromxto10.classList.add('none');
+    form10tox.classList.add('none');
+    registration.classList.remove('none');
+    toX.classList.remove('select__button-selected');
+    to10.classList.remove('select__button-selected');
+    toReg.classList.add('select__button-selected');
+})
+
+const formElement1 = new Form ('#from10-x', '#fromx-10');
+const a = formElement1.renderJson();
+console.log(a);
+
+fromxto10.addEventListener('submit', (evt) => {
     evt.preventDefault();
-    const quantityValue = nonbinaryForm.querySelector('#quantity').value;
-    const maxValue = nonbinaryForm.querySelector('#max').value;
-    const systemsValue = nonbinaryForm.querySelector('#systems').value;
-    const formElement = new Form(quantityValue, maxValue, systemsValue);
+    const formElement = new Form ('#from10-x', '#fromx-10');
     const json = formElement.renderJson();
+
     console.log(json);
-    fetch('http://13222a0a5e76.ngrok.io/gen', {
-        method: 'POST',
-        body: json
-    })
+
+    api.getToX(json)
     .then((res) => {
         return res.json();
     })
@@ -23,10 +62,26 @@ nonbinaryForm.addEventListener('submit', (evt) => {
     .catch((err) => {
         console.log(err);
     })
-    // $.post('http://13222a0a5e76.ngrok.io/gen', json).success((resp) => {
-    //     if (!resp.success) {
-    //         return
-    //     }
-    //     console.log(resp.text);
-    // })
+})
+
+
+form10tox.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const formElement = new Form ('#from10-x', '#fromx-10');
+    formElement.log();
+    const json = formElement.renderJson();
+
+    console.log(json);
+
+    api.getToX(json)
+    .then((res) => {
+        return res.json();
+    })
+    .then((res) => {
+        if (!res.success) {console.log("UNSUCCESSFUL RESPONSE!!!!!!!@!@11@@!11!!!")}
+        formAnswer.innerHTML = res.text.toString().replaceAll('\n', '<br>');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 });
