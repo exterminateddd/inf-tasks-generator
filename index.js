7893ace66a9be0cc859f37353743e6a6640b4485
alex <fennyflop@gmail.com>
2021-04-06 13:57:16 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,87 +1,63 @@
-import FormQ from './FormQ.js';
-import Form from './Form.js';
-import Api from './Api.js';
-const form10tox = document.querySelector('#from10-x');
-const fromxto10 = document.querySelector('#fromx-10');
+import FormQ from './components/FormQ.js';
+import Form from './components/Form.js';
+import Api from './components/Api.js';
+const mainForm = document.querySelector('#main-form');
 const registration = document.querySelector('#registration');
-const toX = document.querySelector('#toX');
-const to10 = document.querySelector('#to10');
-const toReg = document.querySelector('#toReg');
-const formAnswer = form10tox.querySelector('.form__answer');
+const formAnswer = document.querySelector('.answer__message');
+const main = document.querySelector('.main');
+const answer = document.querySelector('.answer');
+const answersLink = document.querySelector('#answers');
+const questionsLink = document.querySelector('#questions');
+const notification = document.querySelector('.notification');
+const notificationMessage = document.querySelector('.notification__message');
+const response = document.querySelector('#response');
 
 const api = new Api ({
-    baseUrl: 'http://0c1b8c566344.ngrok.io',
-    
+    baseUrl: 'http://908e393668c6.ngrok.io',
 });
 
-toX.addEventListener('click', () => {
-    form10tox.classList.remove('none');
-    registration.classList.add('none');
-    fromxto10.classList.add('none');
-    toX.classList.add('select__button-selected');
-    to10.classList.remove('select__button-selected');
-    toReg.classList.remove('select__button-selected');
-});
+function displayError () {
+    response.classList.add('none');
+    notification.backgroundColor = 'crimson';
+    notificationMessage.textContent = 'Неудачно :('
+}
 
-to10.addEventListener('click', () => {
-    fromxto10.classList.remove('none');
-    form10tox.classList.add('none');
-    registration.classList.add('none');
-    toX.classList.remove('select__button-selected');
-    to10.classList.add('select__button-selected');
-    toReg.classList.remove('select__button-selected');
-});
+function displayAnswer (res) {
+    const currentTime = new Date();
+    const time = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`
+    const ans = res.text;
+    const noAns = res.text_no_ans;
+    let blob = new Blob([noAns], {type: 'text/plain'});
+    let blobAnswers = new Blob([ans], {type: 'text/plain'});
+    answersLink.addEventListener('click', () => {
+        answersLink.setAttribute('href', URL.createObjectURL(blobAnswers));
+        answersLink.setAttribute('download', 'answers');
+    })
+    questionsLink.addEventListener('click', () => {
+        questionsLink.setAttribute('href', URL.createObjectURL(blob));
+        questionsLink.setAttribute('download','questions');
+    });
+    notification.classList.remove('none');
+    notificationMessage.textContent = `Успешно! ${time}`;
+}
 
-toReg.addEventListener('click', () => {
-    fromxto10.classList.add('none');
-    form10tox.classList.add('none');
-    registration.classList.remove('none');
-    toX.classList.remove('select__button-selected');
-    to10.classList.remove('select__button-selected');
-    toReg.classList.add('select__button-selected');
-})
 
-const formElement1 = new Form ('#from10-x', '#fromx-10');
-const a = formElement1.renderJson();
-console.log(a);
 
-fromxto10.addEventListener('submit', (evt) => {
+mainForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
+    console.log('a')
     const formElement = new Form ('#from10-x', '#fromx-10');
     const json = formElement.renderJson();
 
     console.log(json);
-
     api.getToX(json)
     .then((res) => {
         return res.json();
     })
     .then((res) => {
-        formAnswer.innerHTML = res.text.toString().replaceAll('\n', '<br>');
+        displayAnswer(res);
     })
     .catch((err) => {
         console.log(err);
     })
 })
-
-
-form10tox.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    const formElement = new Form ('#from10-x', '#fromx-10');
-    formElement.log();
-    const json = formElement.renderJson();
-
-    console.log(json);
-
-    api.getToX(json)
-    .then((res) => {
-        return res.json();
-    })
-    .then((res) => {
-        if (!res.success) {console.log("UNSUCCESSFUL RESPONSE!!!!!!!@!@11@@!11!!!")}
-        formAnswer.innerHTML = res.text.toString().replaceAll('\n', '<br>');
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-});
