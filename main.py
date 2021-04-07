70bb466c3a31a805cf469132d997e4b4abad4641
exterminateddd <50239930+exterminateddd@users.noreply.github.com>
2021-04-07 13:14:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,77 +1,51 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
-from random import choice, randint
+from generator import generate_all
 
 app = Flask(__name__)
 CORS(app)
 
-D = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-
-def convert(n, to_base=10, from_base=10):
-    if isinstance(n, str):
-        n = int(n, from_base)
-    if n >= to_base:
-        return convert(n // to_base, to_base) + D[n % to_base]
-    else:
-        return D[n]
-
-
-def generate_from_10(questions: int, max_num: int, systems: list):
-    systems = [int(i) for i in systems]
-    text = []
-    for i in range(0, questions):
-        cur_task = ''
-        n = randint(1, max_num)
-        from_base = 10
-        to_base = randint(2, choice(systems))
-        cur_task = f"{i}. {n} из {from_base} в {to_base}: _____ (ОtBеt: {convert(n, to_base, from_base)})</br>"
-        text.append(cur_task)
-    return text
-
-
-def generate_to_10(questions: int, max_num: int, systems: list):
-    systems = [int(i) for i in systems]
-    text = []
-    for i in range(0, questions):
-        cur_task = ''
-        n = randint(1, max_num)
-        from_base = randint(2, choice(systems))
-        to_base = 10
-        cur_task = f"{i}. {n} из {from_base} в {to_base}: _____ (ОtBеt: {convert(n, to_base, from_base)})</br>"
-        text.append(cur_task)
-    return text
-
-
-def generate_all(questions_to10, max_num_to10, systems_to10, questions_from10, max_num_from10, systems_from10):
-    return '\n'.join(generate_to_10(questions_to10, max_num_to10, systems_to10)) + '</br>From 10</br>' + \
-           '\n'.join(generate_from_10(questions_from10, max_num_from10, systems_from10))
+BR = '</br>'
 
 
 @app.route('/gen', methods=['POST'])
 def gen():
+    json__ = {
+        'msg': 'Empty',
+        'success': True,
+        'text': '',
+        'text_no_ans': ''
+    }
     try:
         json_ = request.get_json(force=True)
         if not json_:
             raise KeyError
-        max_to10 = json_['to10']['maxNum']
-        systems_to10 = json_['to10']['systems']
-        questions_to10 = json_['to10']['questions']
-        max_from10 = json_['from10']['maxNum']
-        systems_from10 = json_['from10']['systems']
-        questions_from10 = json_['from10']['questions']
+        variants: int = json_['variants']
+        max_to10: int = json_['to10']['maxNum']
+        systems_to10: list = json_['to10']['systems']
+        questions_to10: int = json_['to10']['quantity']
+        max_from10: int = json_['from10']['maxNum']
+        systems_from10: list = json_['from10']['systems']
+        questions_from10: int = json_['from10']['quantity']
     except Exception as e:
-        print(e.args)
-        return jsonify({
-            'success': False
-        })
-    text = generate_all(questions_to10, max_to10, systems_to10, questions_from10, max_from10, systems_from10)
-    print(text)
-    return jsonify({
-        'success': True,
-        'text': text
-    })
+        json__['success'] = False
+        json__['msg'] = 'Error: '+BR+str(type(e).__name__)+': '+str(e.args)
+        return json__
+    print(json_)
+    no_ans_ret = ''
+    ans_ret = ''
+    for v in range(variants):
+        generate = generate_all(questions_to10, max_to10, systems_to10, questions_from10, max_from10, systems_from10)
+        no_ans_ret += f'{BR}ВАРИАНТ №{v+1}'+BR+'в 10-чную'+BR+generate['gto10']['no_ans']+BR+'в х-ной'+BR+generate['gfrom10']['no_ans']
+        ans_ret += f'{BR}ВАРИАНТ №{v+1}'+BR+'в 10-чную'+BR+generate['gto10']['ans']+BR+'в х-ной'+BR+generate['gfrom10']['ans']
+    json__['msg'] = 'Success!'
+    json__['text'] = ans_ret.replace(BR, '\n')
+    json__['text_no_ans'] = no_ans_ret.replace(BR, '\n')
+    # if input(str(json_)+'\nSend?').lower() in ['yes', 'y', 'send', 'true', 'accept']: pass
+    # else: json__['msg'] = 'Unaccepted request parameters.'; json__['success'] = False
+    return jsonify(json__)
 
 
 if __name__ == '__main__':
-    app.run(port=2000)
+    app.run(port=2000, debug=True)
